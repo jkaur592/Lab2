7c327c7ab30c43495457547a9f53286fab2c29f3
jkaur592 <159809275+jkaur592@users.noreply.github.com>
2024-02-12 12:59:31 -0500

    Create Lab2.py

---
+++ Lab2.py
@@ -0,0 +1,13 @@
+import sys
+
+def print_args():
+# Print the script name
+print("Script name:", sys.argv[0])
+
+# Check if arguments were provided
+if len(sys.argv) > 1:
+    # Print the script name along with provided arguments
+    print("Script name and variables:", ' '.join(sys.argv))
+
+ # Call the function
+print_args()
